6d2b063b8d23fd220eb0f14232a3d996c08845c6
Quescc <42616677+Quescc@users.noreply.github.com>
2023-12-25 18:12:44 +0800

    Add files via upload

--- src/main/java/top/sakta/hrmsys/service/impl/EmployeeServiceImpl.java
+++ src/main/java/top/sakta/hrmsys/service/impl/EmployeeServiceImpl.java
@@ -6,6 +6,7 @@ import top.sakta.hrmsys.domain.Employee;
 import top.sakta.hrmsys.mapper.EmployeeMapper;
 import top.sakta.hrmsys.service.EmployeeService;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -38,4 +39,26 @@ public class EmployeeServiceImpl implements EmployeeService {
     public int deleteEmployee(String rID) {
         return employeeMapper.deleteEmployee(rID);
     }
+
+    @Override
+    public List<Employee> getEmployeeLikeID(String ID) {
+        return employeeMapper.getEmployeeLikeID(ID);
+    }
+
+    @Override
+    public List<Employee> getEmployeesByStatus(int eStatus) {
+        return employeeMapper.getEmployeesByStatus(eStatus);
+    }
+
+    @Override
+    public List<Employee> getEmployeesByConditions(String eL1InstID, String eL2InstID, String eL3InstID, String ePositionCategory, String ePositionName, Date StartRecodDate, Date EndRecodDate) {
+        return employeeMapper.getEmployeesByConditions(eL1InstID,eL2InstID,eL3InstID,ePositionCategory,ePositionName,StartRecodDate,EndRecodDate);
+    }
+
+    @Override
+    public int updateEmployeeStatus(String eID, int eStatus) {
+        return employeeMapper.updateEmployeeStatus(eID,eStatus);
+    }
+
+
 }
