aca0d31f3e7d76f531ca6ed07be3947d003e6347
Quescc <42616677+Quescc@users.noreply.github.com>
2023-12-28 00:28:42 +0800

    Add files via upload

--- src/main/java/top/sakta/hrmsys/controller/ScheduledSendedController.java
+++ src/main/java/top/sakta/hrmsys/controller/ScheduledSendedController.java
@@ -5,12 +5,11 @@ import cn.dev33.satoken.util.SaResult;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import top.sakta.hrmsys.domain.ScheduledSended;
 import top.sakta.hrmsys.service.impl.ScheduledSendedServiceImpl;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -29,4 +28,21 @@ public class ScheduledSendedController {
         return SaResult.ok().setData(allScheduledSendeds);
     }
 
+    @SaCheckPermission("bonus.update")
+    @Operation(summary = "更新/添加定时任务执行记录", description = "参数pID")
+    @PutMapping("/updata/{pID}")
+    public SaResult updateScheduledSended(@PathVariable Integer pID){
+        ScheduledSended scheduledSended = scheduledSendedService.getScheduledSendedByID(pID);
+        Date date = new Date();
+        if(scheduledSended == null){
+            scheduledSended = new ScheduledSended();
+            scheduledSended.setLastExecutionTime(date);
+            scheduledSended.setPID(pID);
+            scheduledSendedService.insertScheduledSended(scheduledSended);
+        }else{
+            scheduledSended.setLastExecutionTime(date);
+            scheduledSendedService.updateScheduledSended(scheduledSended);
+        }
+        return SaResult.ok("更新成功");
+    }
 }
